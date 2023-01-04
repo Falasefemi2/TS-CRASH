fe08b6ce9409c557ba3680a0f60fff728787184f
Falase Femi <femifalase228@gmail.com>
2023-01-04 08:56:40 +0100

    Learn types in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,43 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = "FALASE";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+// Tuple
+let person = [1, "femi", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "femi"],
+    [2, "sam"],
+    [3, "ayo"],
+];
+// Union: to hold more than one types
+let pid = 22;
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2);
+const user = {
+    id: 1,
+    name: "femi",
+};
+// Type Assertion: To treat an entity as a diffrent type
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,55 @@
-let id:number = 5;
+// Basic Types
 
+let id: number = 5;
+let company: string = "FALASE";
+let isPublished: boolean = true;
+let x: any = "Hello";
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "Hello"];
 
+// Tuple
+let person: [number, string, boolean] = [1, "femi", true];
+// Tuple Array
+let employee: [number, string][];
 
+employee = [
+  [1, "femi"],
+  [2, "sam"],
+  [3, "ayo"],
+];
+
+// Union: to hold more than one types
+let pid: string | number = 22;
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "left",
+  Right = "Right",
+}
+
+console.log(Direction2);
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+  id: 1,
+  name: "femi",
+};
+
+// Type Assertion: To treat an entity as a diffrent type
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
